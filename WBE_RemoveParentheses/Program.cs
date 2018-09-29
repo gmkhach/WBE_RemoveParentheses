71aaa80ecc354e4b00e6d3914e507971c0820962
Gabriel Khachatryan <gmkhach@gmail.com>
2018-09-28 23:40:41 -0400

    brute-force method implemented

--- WBE_RemoveParentheses/Program.cs
+++ WBE_RemoveParentheses/Program.cs
@@ -1,8 +1,14 @@
-﻿using System;
+﻿/**
+ * Write a function that accepts a string, removes all characters between parentheses, then returns the modified string
+ * ie: "one (two) three" should return "one () three"
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections;
 
 namespace WBE_RemoveParentheses
 {
@@ -10,6 +16,45 @@ namespace WBE_RemoveParentheses
     {
         static void Main(string[] args)
         {
+            do
+            {
+                try
+                {
+                    Console.Write("Input a string\n\n>>> ");
+                    string input = Console.ReadLine();
+                    Console.WriteLine(RemoveParentheses(input));
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("\n" + ex.Message);
+                }
+                Console.Write("\nPress Enter to try another string...");
+                Console.ReadLine();
+                Console.Clear();
+            } while (true);
+        }
+
+        static string RemoveParentheses(string input)
+        {
+            string output = "";
+            bool include = true;
+            Queue myQ = new Queue();
+            foreach (var ch in input)
+            {
+                if (ch == '(')
+                {
+                    include = false;
+                }
+                else if (ch == ')')
+                {
+                    include = true;
+                }
+                if (include)
+                {
+                    output += ch;
+                }
+            }
+            return output;
         }
     }
 }
